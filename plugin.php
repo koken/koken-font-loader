7f83e107454ce17dff0566f1dcf9e54d135adfa4
Lauren Smith <lauren@slideshowpro.net>
2013-02-26 12:58:44 -0600

    provide fallback to local webfont.js file

--- plugin.php
+++ plugin.php
@@ -10,13 +10,13 @@ class KokenFontLoader extends KokenPlugin {
 
 	function render()
 	{
-	
+
 		$output = (object) array();
-	
+
 		if (!empty($this->data->typekit)) {
 			$output->typekit = (object) array('id' => $this->data->typekit);
 		}
-		
+
 		if (!empty($this->data->google)) {
 			$families = array();
 			$parts = explode(',',$this->data->google);
@@ -25,19 +25,21 @@ class KokenFontLoader extends KokenPlugin {
 			}
 			$output->google = (object) array('families' => $families);
 		}
-		
+
 		if (!empty($this->data->fontdeck)) {
 			$output->fontdeck = (object) array('id' => $this->data->fontdeck);
 		}
-		
+
 		if (!empty($this->data->fontscom)) {
 			$output->monotype = (object) array('projectId' => $this->data->fontscom);
 		}
-		
+
 		$output = json_encode($output);
-		
+		$fallback = 'document.write(\'<script src="'.$this->get_path().'/webfont.js">\x3C/script>\')';
+
 		echo <<<OUT
 <script src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
+<script>window.WebFont || $fallback</script>
 <script type="text/javascript">WebFont.load($output);</script>
 OUT;
 
